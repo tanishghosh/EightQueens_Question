74228954ad0d64b6116c2a1b1a786d0fc14018c8
tanishghosh <tanishghosh@gmail.com>
2021-07-08 10:38:01 +0530

    modified ChessBoardRenderer and GameStrategy

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,18 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		
+		int lineNumber = square/8;
+		
+		if(lineNumber%2==0)
+			if(square%2==0)
+				return false;
+			else
+				return true;
+		else
+			if(square%2==0)
+				return true;
+			else
+				return false;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -1,22 +1,33 @@
 public class GameStrategy {
 	private boolean placedQueens[][] = new boolean[8][8];
 	private int numQueens = 0;
+	
+	GameStrategy(){
+		
+		for(int i=0;i<8;i++)
+			for(int j=0;j<8;j++)
+				placedQueens[i][j] = false;
+		
+	}
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		
+		int cellColumnId = (cellId%8);
+		
+		return cellColumnId;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
 		
-		return 0;	// just for the heck of it
+		int cellRowId = (cellId/8);
+		
+		return cellRowId;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
-		boolean isValid = true;
-
+		
 		if(numQueens == 8) throw new GameOverException();	// just return out of the method
 
 		// the game is still on, so let us continue...
@@ -29,7 +40,35 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
-		return isValid;
+
+	        int i, j;
+	  
+	        /* Conditional check for preventing two queens in single row 
+	         * given that the second queen is tried to be placed
+	         * at left of the first queen
+	         * */
+	        for (i = 0; i < col; i++)
+	            if (placedQueens[row][i] == true)
+	            		return false;
+	        
+	        /* Conditional check for preventing two queens in single column */
+	        for (i = 0; i < row; i++)
+	            if (placedQueens[i][col] == true)
+	            		return false;
+	  
+	        /* Conditional check for queens on upper diagonal on left side */
+	        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+	            if (placedQueens[i][j] == true)
+            		return false;
+	  
+	        /* Conditional check for queens on lower diagonal on left side */
+	        for (i = row, j = col; j >= 0 && i <=7 ; i++, j--)
+	            if (placedQueens[i][j] == true)
+            		return false;
+	        
+	        placedQueens[row][col] = true;
+	        numQueens+=1;
+	        return true;
 	}
 }
 
